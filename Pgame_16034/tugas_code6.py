1d9aeceb2cfa3e2595f98c952de9cad328891522
WUYUN ABD HAMID <55941644+WuyunHamid@users.noreply.github.com>
2019-11-18 21:47:28 +0900

    Latihan : Modul_Praktikum_5

---
+++ Pgame_16034/tugas_code6.py
@@ -0,0 +1,17 @@
+# Tugas_code6 : Menggunakan Kondisional
+
+nilai = 200
+if nilai > 80:		# menggunakan kondisional if
+    print ("Sangat Baik")
+
+nilai = 80
+if nilai > 65 :		# menggunakan kondisional if
+    print ("Cukup Baik")
+
+nilai = 65
+if nilai > 50 :		# menggunakan kondisional if
+    print ("Baik")
+
+nilai = 45
+if nilai > 35 :		# menggunakan kondisional if
+    print ("Kurang")
